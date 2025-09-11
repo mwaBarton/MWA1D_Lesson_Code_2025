dbe1acd00ff02f0f343551b91f78a21b72c9a3b0
mwa <mwa@barton.ac.uk>
2025-09-11 10:15:11 +0100

    L101

---
+++ L101 - If/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L101___If
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your favourite number: ");
+            int x = int.Parse(Console.ReadLine());
+
+            if (x == 13)
+            {
+                Console.WriteLine("That's the same as Charlie's!");
+            }
+            else if (x < 13)
+            {
+                Console.WriteLine("That's less than Charlie's");
+            }
+            else
+            {
+                Console.WriteLine("That's more than Charlie's");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
