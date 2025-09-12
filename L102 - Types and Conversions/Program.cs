4a8933fe85ea6c33869c061190d122e7dd335b81
mwa <mwa@barton.ac.uk>
2025-09-12 15:59:35 +0100

    L102

---
+++ L102 - Types and Conversions/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L102___Types_and_Conversions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int myNum = (int) Math.Round(6.5,MidpointRounding.AwayFromZero);
+
+            //int yourNum = (int) (myNum * 2.1);
+
+            Console.WriteLine(myNum);
+
+            string name = "Max";
+
+            char c = name[2];
+            c = 'g';
+
+            bool b = false;
+            Console.Write("Are you sure? ");
+            string input = Console.ReadLine();
+
+            //if (input == "yes")
+            //{
+            //    b = true;
+            //} else
+            //{
+            //    b = false;
+            //}
+
+            b = input == "yes";
+
+            // Parsing
+            int x = int.Parse("32");
+            double y = double.Parse("6.7");
+            bool z = bool.Parse("True");
+
+            string s;
+
+            s = x.ToString();
+            Console.WriteLine(x);
+
+            Console.WriteLine(c);
+        }
+    }
+}
---
+++ L102 - Types and Conversions/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("L102 - Types and Conversions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Barton Peveril Sixth Form College")]
+[assembly: AssemblyProduct("L102 - Types and Conversions")]
+[assembly: AssemblyCopyright("Copyright © Barton Peveril Sixth Form College 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7dfe1f6c-047f-483c-ad3b-dbeaf6f2dd8f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
