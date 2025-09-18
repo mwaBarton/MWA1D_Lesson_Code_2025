1df553bc61424be4950f111aab3c89f6c04b2c65
mwa <mwa@barton.ac.uk>
2025-09-18 09:43:27 +0100

    L104

---
+++ L104 - Switch/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace L104___Switch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter your height (in 60ths of the average height): ");
+            int height = int.Parse(Console.ReadLine());
+
+            switch (height)
+            {
+                case 0:
+                    Console.WriteLine("You are a pancake.");
+                    break;
+                case 60:
+                    Console.WriteLine("average. boring!");
+                    break;
+                default:
+                    Console.WriteLine($"Your height is {height} 60ths.");
+                    break;
+            }
+        }
+    }
+}
