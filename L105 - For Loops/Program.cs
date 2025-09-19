295d5123b73207a00b199c6043cf5b6b8c947243
mwa <mwa@barton.ac.uk>
2025-09-19 15:33:57 +0100

    L105

---
+++ L105 - For Loops/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L105___For_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int bob = -10; bob < 10; bob = bob - 2)
+            {
+                Console.WriteLine(3 * bob);
+            }
+
+            Console.WriteLine("done.");
+        }
+    }
+}
---
+++ L105 - For Loops/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("L105 - For Loops")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Barton Peveril Sixth Form College")]
+[assembly: AssemblyProduct("L105 - For Loops")]
+[assembly: AssemblyCopyright("Copyright © Barton Peveril Sixth Form College 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("cdf4b696-a3e7-44e1-98cf-589ec2ee3de6")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
