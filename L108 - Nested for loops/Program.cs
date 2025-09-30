d05ee3f8d3d0c20fa33a0e3d756c82095688e4bd
mwa <mwa@barton.ac.uk>
2025-09-30 11:40:39 +0100

    L108

---
+++ L108 - Nested for loops/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace L108___Nested_for_loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int playerI = 10, playerJ = 10;
+
+
+            while (true)
+            {
+                Console.Clear();
+                for (int i = 0; i < 15; i++)
+                {
+                    for (int j = 0; j < 40; j++)
+                    {
+                        if (i == playerI && j == playerJ)
+                        {
+                            Console.BackgroundColor = ConsoleColor.Red;
+                            Console.Write("O");
+                            Console.BackgroundColor = ConsoleColor.Black;
+                        }
+                        else
+                        {
+                            Console.Write("X");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.ReadKey();
+                playerI += 1;
+            }
+
+
+        }
+    }
+}
