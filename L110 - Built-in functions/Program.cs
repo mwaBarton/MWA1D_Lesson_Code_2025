f2f9ac93689b2fa4f512a3aea54ca3f5777f05f5
mwa <mwa@barton.ac.uk>
2025-10-14 11:54:18 +0100

    L110

---
+++ L110 - Built-in functions/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L110___Built_in_functions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a string: ");
+            string s = Console.ReadLine();
+
+            // Length
+            Console.WriteLine(s.Length);
+
+            // Case
+            string newString = s.ToUpper();
+            Console.WriteLine(newString);
+
+            newString = s.ToLower();
+            Console.WriteLine(newString);
+
+            // Substring 
+            newString = s.Substring(3, 5);
+            Console.WriteLine(newString);
+
+            // Index of
+            Console.WriteLine(s.IndexOf("lo Wo"));
+        }
+    }
+}
