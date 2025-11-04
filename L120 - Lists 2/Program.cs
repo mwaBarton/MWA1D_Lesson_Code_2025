b5a650acbb02c7951ee6c201a3505a3d1106c4f3
mwa <mwa@barton.ac.uk>
2025-11-04 11:52:45 +0000

    L120 working through W109

---
+++ L120 - Lists 2/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace L120___Lists_2
+{
+    internal class Program
+    {
+        // List Printer
+        static void printList(List<string> list)
+        {
+            Console.Write("[");
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.Write(list[i]);
+                if (i < list.Count - 1) Console.Write(", ");
+            }
+            Console.WriteLine("]");
+        }
+
+        // Remove duplicates
+        static void InputListWithNoDuplicates(List<string> list) {
+            string input = "";
+            while (input != "done")
+            {
+                Console.Write("Enter next name, or 'done' to stop: ");
+                input = Console.ReadLine();
+
+                if (input != "done")
+                {
+                    if (list.Contains(input))
+                    {
+                        Console.WriteLine("Duplicate detected and ignored.");
+                    } else
+                    {
+                        list.Add(input);
+                    }
+                }
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            // Basics
+            List<int> myList = new List<int>();
+            myList.Add(6);
+            myList.Add(-1);
+            myList.Add(7);
+            myList.Add(8);
+            myList.Add(60);
+
+            // List Printer
+            List<string> shopping = new List<string> { "Eggs", "Flour", "Milk", "Butter" };
+            printList(shopping);
+
+            // Array to list
+            int[] nums = new int[] { 1, 2, 3, 4, 5, 6 };
+            List<int> numsAsList = new List<int>();
+            foreach (int i in nums)
+            {
+                numsAsList.Add(i);
+            }
+
+            // List to array
+            int[] numsAsListAsArray = new int[numsAsList.Count];
+            for (int i = 0; i < numsAsList.Count; i++)
+            {
+                numsAsListAsArray[i] = numsAsList[i];
+            }
+
+            // Remove duplicates
+            List<string> uniques = new List<string>();
+            InputListWithNoDuplicates(uniques);
+            Console.WriteLine($"There are {uniques.Count} unique names:");
+            printList(uniques);
+
+            // Split string into a list
+            Console.Write("Enter a string of words with spaces in between: ");
+            string phrase = Console.ReadLine();
+            string[] words = phrase.Split(' ');
+            List<string> wordsList = new List<string>();
+            foreach (string word in words)
+            {
+                wordsList.Add(word);
+            }
+            printList(wordsList);
+
+            // List difference
+            Console.Write("Enter another string of words with spaces in between: ");
+            string phrase2 = Console.ReadLine();
+            string[] words2 = phrase2.Split(' ');
+            List<string> wordsList2 = new List<string>();
+            foreach (string word in words2)
+            {
+                wordsList2.Add(word);
+            }
+
+            foreach (string word in wordsList2)
+            {
+                while (wordsList.Contains(word))
+                {
+                    wordsList.Remove(word);
+                }
+            }
+            printList(wordsList);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ L120 - Lists 2/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("L120 - Lists 2")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Barton Peveril Sixth Form College")]
+[assembly: AssemblyProduct("L120 - Lists 2")]
+[assembly: AssemblyCopyright("Copyright © Barton Peveril Sixth Form College 2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("2b7bf1f4-3a67-494f-86ba-5d3b22407058")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
