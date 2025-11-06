afcc07743c06e87b9a0a9da5141509376aa877f8
mwa <mwa@barton.ac.uk>
2025-11-06 10:17:08 +0000

    L121

---
+++ L121 - Parallel Arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L121___Parallel_Arrays
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] Student = new string[] {
+                "Theo",
+                "Josh",
+                "Emily",
+                "Sam"
+            };
+            int[] marks = new int[]
+            {
+                67,
+                42,
+                16,
+                67
+            };
+
+            for (int i = 0; i < Student.Length; i++) {
+                Console.WriteLine($"Student {Student[i]} scored {marks[i]} marks.");            
+            }
+
+            
+        }
+    }
+}
