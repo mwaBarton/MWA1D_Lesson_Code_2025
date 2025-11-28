d4236543313d115eb475489ae49b90cea0115e29
mwa <mwa@barton.ac.uk>
2025-11-28 16:00:37 +0000

    L127

---
+++ L127 - Ref/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L127___Ref
+{
+    internal class Program
+    {
+        static void MyVeryComplicatedSubroutine(List<int>[] nums)
+        {
+            nums[0][0] = 2;
+
+            Console.WriteLine(nums[0][0]);
+        }
+
+        static void Main(string[] args)
+        {
+            List<int>[] x = new List<int>[] { 
+                new List<int>() { 1 }, 
+                new List<int>() { 2 } 
+            };
+
+            List<int>[] xToPass = new List<int>[x.Length];
+            for (int i = 0; i < x.Length; i++)
+            {
+                xToPass[i] = new List<int>();
+                for (int j = 0; j < x[i].Count; j++)
+                {
+                    xToPass[i].Add(x[i][j]);
+                }
+            }
+
+            MyVeryComplicatedSubroutine(xToPass);
+
+            Console.WriteLine(x[0][0]);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ L127 - Ref2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L127___Ref2
+{
+    internal class Program
+    {
+        static void MyVerySimpleToUnderstandSubroutine(ref int num)
+        {
+            num = num + 1;
+
+            Console.WriteLine(num);
+        }
+
+        static void Main(string[] args)
+        {
+            int x = 1;
+
+            MyVerySimpleToUnderstandSubroutine(ref x);
+
+            Console.WriteLine(x);
+
+            Console.ReadKey();
+        }
+    }
+}
