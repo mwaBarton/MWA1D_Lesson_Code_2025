1e426b55874df9a6b05731eaaea8462ec5e9bebb
mwa <mwa@barton.ac.uk>
2026-02-06 15:38:29 +0000

    L150 - Access Modifiers and Constructors

---
+++ L150 - Access Modifiers/Company.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace L150___Access_Modifiers
+{
+    internal class Company
+    {
+        public int numberOfEmployees;
+        private string name;
+        private double profitSoFar;
+
+        public Company(string companyName)
+        {
+            name = companyName;
+        }
+
+        public Company()
+        {
+            name = "Untitled_Company";
+        }
+
+        public Company(int employees, string name)
+        {
+            this.name = name;
+            numberOfEmployees = employees;
+        }
+
+        public string GetName()
+        {
+            return name;
+        }
+
+        public double GetProfitSoFar()
+        {
+            return profitSoFar;
+        }
+
+        public void EmploySomeone()
+        {
+            numberOfEmployees += 1;
+        }
+
+        public void YoureFired()
+        {
+            numberOfEmployees -= 1;
+        }
+
+        public double SellProducts(int numberOfProducts)
+        {
+            double profit = numberOfProducts * (3.0 - 0.01);
+
+            profitSoFar += profit;
+
+            return profit;
+        }
+    }
+}
---
+++ L150 - Access Modifiers/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace L150___Access_Modifiers
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Company business = new Company("Henkel");
+
+            business.numberOfEmployees = 47000;
+
+            business.EmploySomeone();
+            business.YoureFired();
+
+            for (int i = 0; i < 67; i++)
+            {
+                Console.Write($"Day {i+1}: ");
+                double profitToday = business.SellProducts(650000);
+                Console.WriteLine($"£{profitToday} made selling Pritt sticks today");
+            }
+
+            Console.WriteLine($"Final profit for {business.GetName()}: £{business.GetProfitSoFar()}");
+        }
+    }
+}
