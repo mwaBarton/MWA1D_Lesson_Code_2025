73a8a70d9409cf23fb02582fb68a97fce2b42b0b
mwa <mwa@barton.ac.uk>
2026-03-06 15:39:06 +0000

    L157

---
+++ L155 - Inheritance/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L155___Inheritance
+{
+    public class Rectangle
+    {
+        private int width, height;
+        public Rectangle(int inWidth, int inHeight)
+        {
+            width = inWidth;
+            height = inHeight;
+        }
+
+        public Rectangle()
+        {
+            width = 10;
+            height = 10;
+        }
+
+        public virtual int getArea()
+        {
+            return width * height;
+        }
+    }
+
+    public class Square : Rectangle
+    {
+        public string name;
+
+        public Square(int sideLength, string inName) : base(sideLength, sideLength)
+        {
+            name = inName;
+        }
+
+        public override int getArea()
+        {
+            return name.Length;
+        }
+    }
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle(3, 4);
+
+            Rectangle r2 = new Square(5, "Jonathan");
+
+            Square itsActuallyASquare = (Square) r2;
+
+
+            Console.WriteLine(r2.getArea());
+            Console.WriteLine(itsActuallyASquare.getArea());
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ L157 - Subtype Polymorphism/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L157___Subtype_Polymorphism
+{
+    public class Shape
+    {
+        public virtual double getArea()
+        {
+            return 0;
+        }
+    }
+
+    public class Square : Shape
+    {
+        protected int side;
+
+        public Square(int sideLength)
+        {
+            side = sideLength;
+        }
+
+        public override double getArea()
+        {
+            return side * side;
+        }
+    }
+
+    public class SillySquare : Square
+    {
+        public SillySquare(int sideLength) : base(sideLength)
+        {
+        }
+
+        public override double getArea()
+        {
+            return side * side + 1;
+        }
+    }
+
+    public class Circle : Shape
+    {
+        private int radius;
+        
+        public Circle(int inRadius)
+        {
+            radius = inRadius;
+        }
+
+        public override double getArea()
+        {
+            return radius * radius * Math.PI;
+        }
+    }
+
+
+    internal class Program
+    {
+
+        static void PrintShapeArea(Shape s)
+        {
+            Console.WriteLine(s.getArea());
+        }
+
+        static void Main(string[] args)
+        {
+            List<Shape> shapes = new List<Shape>();
+
+            shapes.Add(new Square(4));
+            shapes.Add(new Square(5));
+            shapes.Add(new Square(6));
+            shapes.Add(new Circle(4));
+            shapes.Add(new SillySquare(4));
+
+
+            foreach (Shape shape in shapes)
+            {
+                PrintShapeArea(shape);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
